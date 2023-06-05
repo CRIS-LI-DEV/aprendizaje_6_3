501c2b49c99775b1409d0f96c49c486bd490f078
CRIS-LI-DEV <131208714+CRIS-LI-DEV@users.noreply.github.com>
2023-06-05 00:06:14 -0400

    Aprendizaje 6 3

--- proyecto_django/nuevo_proyecto/nuevo_proyecto/urls.py
+++ proyecto_django/nuevo_proyecto/nuevo_proyecto/urls.py
@@ -5,8 +5,8 @@ from app1.views import  perfil_usuario, ingresar_datos_view,home
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('usuarios/', perfil_usuario, name='usuarios'),
-    path('ingresar-datos/<int:user_id>/', ingresar_datos_view, name='ingresar-datos'),
-    path('', home),
+    path('ingresar-datos/<int:user_id>/', ingresar_datos_view, name='ingresar-datos')
+  
 
      
 
